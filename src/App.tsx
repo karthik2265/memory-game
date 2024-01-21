0a15ed224d683ef8ff820f7ca5fc3071814d18a9
karthik2265 <63234167+karthik2265@users.noreply.github.com>
2024-01-21 20:25:39 +0530

    add icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
-import "./App.css";
+import Anchor from "./components/icons/Anchor";
 import { ThemeProvider } from "./context/ThemeContext";
 
 function App() {
-  return <ThemeProvider>hi</ThemeProvider>;
+  return (
+    <ThemeProvider>
+      <Anchor></Anchor>
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ src/components/icons/Football.tsx
@@ -0,0 +1,9 @@
+const Football = () => {
+  return (
+    <svg width="56" height="56" viewBox="0 0 56 56" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <rect width="56" height="56" fill="#304859" />
+    </svg>
+  );
+};
+
+export default Football;
